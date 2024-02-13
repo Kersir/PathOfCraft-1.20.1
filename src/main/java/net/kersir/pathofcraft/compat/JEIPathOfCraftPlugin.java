cb6a230f6acbc0d327df92cf5592cc9222e1d197
Maksim Golubkov <maksgol55@gmail.com>
2024-02-13 20:30:48 +0300

    add JEI compatibility

---
+++ src/main/java/net/kersir/pathofcraft/compat/JEIPathOfCraftPlugin.java
@@ -0,0 +1,32 @@
+package net.kersir.pathofcraft.compat;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.kersir.pathofcraft.PathOfCraft;
+import net.minecraft.resources.ResourceLocation;
+
+@JeiPlugin
+public class JEIPathOfCraftPlugin implements IModPlugin {
+    @Override
+    public ResourceLocation getPluginUid() {
+        return new ResourceLocation(PathOfCraft.MOD_ID, "jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        IModPlugin.super.registerCategories(registration);
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        IModPlugin.super.registerRecipes(registration);
+    }
+
+    @Override
+    public void registerGuiHandlers(IGuiHandlerRegistration registration) {
+        IModPlugin.super.registerGuiHandlers(registration);
+    }
+}
